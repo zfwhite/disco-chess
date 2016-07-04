8052978842883f00a45c85719812de13d327b87d
Zachary White <zfwhite91@gmail.com>
2016-07-04 14:12:47 -0700

    Game can now be loaded on node server. #4

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/default.js', function(req, res) {
+  res.sendFile(__dirname + '/default.js');
+});
+
+app.get('/default.css', function(req, res) {
+  res.sendFile(__dirname + '/default.css');
+});
+
+app.listen(3000);
