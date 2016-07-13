24b8ee28cd074526e6f6c02a22ed52855cb1014b
Zachary White <zfwhite91@gmail.com>
2016-07-13 09:44:27 -0700

    Moved chat function to default.js. #13

--- public/default.js
+++ public/default.js
@@ -1,3 +1,21 @@
+var socket = io();
+
+$('form').submit(function() {
+  socket.emit('chat message', $('#typed').val());
+  $('#typed').val('');
+  return false;
+});
+
+socket.on('chat message', function(msg) {
+  $('#messages').append($('<li>').text(msg));
+  updateScroll();
+});
+
+function updateScroll() {
+  var chat = document.getElementById('messages');
+  chat.scrollTop = chat.scrollHeight;
+}
+
 var sprite = {
   whiteking: '&#9812;',
   whitequeen: '&#9813;',
